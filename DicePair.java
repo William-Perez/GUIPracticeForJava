d2b2251b3b9998dbd5365e91fcb9cdcce68d4f87
William-Perez <35746875+William-Perez@users.noreply.github.com>
2018-03-22 17:14:50 -0700

    Update DicePair.java

--- DicePair.java
+++ DicePair.java
@@ -1,1 +1,56 @@
+package pairOfDice.copy;
 
+public class  DicePair extends Die
+{
+	//member variables
+	private int facevalueDie2; // second die
+	
+	//constructor
+	public DicePair(){super();
+	facevalueDie2 = 1;}
+	
+	//member functions
+	public void setFaceValues(int value1, int value2) 
+	{
+		setFaceValue(value1); //die1
+		facevalueDie2 = value2;//die 2
+	}
+	
+	public int getFaceValue1() 
+	{
+		return getFaceValue();
+	}
+	
+	public int getFaceValue2() 
+	{
+		return facevalueDie2;
+	}
+	
+	public String toString()
+	{
+		String result2 = Integer.toString(facevalueDie2);
+		String result = "The value for die 1 is: " + super.toString() + " \n and the value for die 2 is: " + result2;
+		
+		return result;
+	}
+	
+	public int rollDice1() 
+	{
+		return super.roll();
+	}
+	
+	public int rollDice2()
+	{
+		facevalueDie2 = (int)(Math.random() * MAX) + 1;
+		return facevalueDie2;
+	}
+	
+	public int sumDie() 
+	{
+		int sum = facevalueDie2 + super.faceValue;
+		return sum;
+		
+	}
+	
+	
+}//end class
